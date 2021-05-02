675c3d07ffa4d67f7d1c4ac2c386a892149ae4c6
Farzan <farzan_khoobchehr@yahoo.com>
2021-05-02 16:59:35 -0700

    Revert "UI Completed and adopt implicit intent to achieve activity switch(prepared for activity hijacking)"
    This reverts commit 6f6b1850f16948ec25b4a4c64bb5dc67555a9227.

--- app/src/main/java/com/sample/bankapp/MainActivity.java
+++ app/src/main/java/com/sample/bankapp/MainActivity.java
@@ -2,10 +2,7 @@ package com.sample.bankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,31 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button login = (Button)findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction("withdraw");
-                intent.addCategory("android.intent.category.DEFAULT");
-
-                startActivity(intent);
-            }
-        });
-
-        findViewById(R.id.signUpTv).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction("signUp");
-                intent.addCategory("android.intent.category.DEFAULT");
-
-                startActivity(intent);
-            }
-        });
     }
-
 }
