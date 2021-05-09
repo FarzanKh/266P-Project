594f9d62b4dbf6345bd87920d800b8b6f4fa0159
Mengshi24 <l806685641@gmail.com>
2021-05-08 21:30:25 -0700

    3 Vul Done

--- app/src/main/java/com/sample/bankapp/MainActivity.java
+++ app/src/main/java/com/sample/bankapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.sample.bankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,6 +15,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},1);
+        }
         // remove bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -40,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         passwordEt = findViewById(R.id.passwordSignIn);
         SignInButton = findViewById(R.id.signInBtn);
         SignUpBtn = findViewById(R.id.signUpBtn);
+        Button callButton = findViewById(R.id.callSupport);
 //        progressDialog = new ProgressDialog(this);
 
         SignInButton.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +61,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("Phonenumber", "8582038129");
+                intent.setAction("CallService");
+                intent.addCategory("android.intent.category.DEFAULT");
+                startActivity(intent);
                 finish();
             }
         });
@@ -77,7 +97,11 @@ public class MainActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
                         Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+                        // startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+                        Intent intent = new Intent();
+                        intent.setAction("Dashboard");
+                        intent.addCategory("android.intent.category.DEFAULT");
+                        startActivity(intent);
                         finish();
                     } else {
                         Toast.makeText(MainActivity.this, "Sign In fail!", Toast.LENGTH_LONG).show();
